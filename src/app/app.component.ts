a0a2db81e28d7feae38aca1d27dd9d5721c76ed1
Koimi YOVO <yovoedem33@gmail.com>
2020-09-05 21:14:15 +0200

    Utilisation des Observable avec rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit, OnDestroy {
+
+  seconds: number;
+  counterSubscription: Subscription;
+
   constructor() {}
+
+  ngOnInit() {
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value: number) => {
+        this.seconds = value;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
 }
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,50 +1,63 @@
+import { Subject } from 'rxjs';
+
 export class AppareilService {
-    appareils = [
-        {
-          id: 1,
-          name: 'Machine à laver',
-          status: 'éteint'
-        },
-        {
-          id: 2,
-          name: 'Frigo',
-          status: 'allumé'
-        },
-        {
-          id: 3,
-          name: 'Ordinateur',
-          status: 'éteint'
-        },
-    ];
 
-    getAppareilById(id: number) {
-        const appareil = this.appareils.find(
-          (appareilObject) => {
-            return appareilObject.id === id;
-          }
-        );
-        return appareil;
-    }
+  appareilSubject = new Subject<any[]>();
 
-    switchOnAll() {
-        // tslint:disable-next-line:prefer-const
-        for (let appareil of this.appareils) {
-            appareil.status = 'allumé';
+  private appareils = [
+      {
+        id: 1,
+        name: 'Machine à laver',
+        status: 'éteint'
+      },
+      {
+        id: 2,
+        name: 'Frigo',
+        status: 'allumé'
+      },
+      {
+        id: 3,
+        name: 'Ordinateur',
+        status: 'éteint'
+      },
+  ];
+
+  emitAppareilSubject() {
+    this.appareilSubject.next(this.appareils.slice());
+  }
+
+  getAppareilById(id: number) {
+      const appareil = this.appareils.find(
+        (appareilObject) => {
+          return appareilObject.id === id;
         }
-    }
+      );
+      return appareil;
+  }
 
-    switchOffAll() {
-        // tslint:disable-next-line:prefer-const
-        for (let appareil of this.appareils) {
-            appareil.status = 'éteint';
-        }
+  switchOnAll() {
+    // tslint:disable-next-line:prefer-const
+    for (let appareil of this.appareils) {
+        appareil.status = 'allumé';
     }
+    this.emitAppareilSubject();
+  }
 
-    switchOnOne(index: number) {
-        this.appareils[index].status = 'allumé';
+  switchOffAll() {
+    // tslint:disable-next-line:prefer-const
+    for (let appareil of this.appareils) {
+        appareil.status = 'éteint';
     }
+    this.emitAppareilSubject();
+  }
 
-    switchOffOne(index: number) {
-        this.appareils[index].status = 'éteint';
-    }
+  switchOnOne(index: number) {
+    this.appareils[index].status = 'allumé';
+    this.emitAppareilSubject();
+  }
+
+  switchOffOne(index: number) {
+    this.appareils[index].status = 'éteint';
+    this.emitAppareilSubject();
+  }
 }
